8c8b0e8690a7937f60002d95a0836d78b9852da8
Cesar Castro <cesarcastrocesar@gmail.com>
2021-09-15 23:36:47 -0300

    Bundle web.js with webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+// Webpack uses this to work with directories
+import * as path from 'path';
+
+// This is the main configuration object.
+// Here, you write different options and tell Webpack what to do
+export default {
+
+    // Path to your entry point. From this file Webpack will begin its work
+    entry: './src/ui.js',
+  
+    // Path and filename of your result bundle.
+    // Webpack will bundle all JavaScript into this file
+    output: {
+      path: path.resolve('', 'dist'),
+      publicPath: '',
+      filename: 'web.js'
+    },
+  
+    // Default mode for Webpack is production.
+    // Depending on mode Webpack will apply different things
+    // on the final bundle. For now, we don't need production's JavaScript 
+    // minifying and other things, so let's set mode to development
+    mode: 'production'
+  };
