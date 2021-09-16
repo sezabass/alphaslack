a8cb2bdc7de7db6dee63f4cbc3f4cceae4010645
Cesar Castro <cesarcastrocesar@gmail.com>
2021-09-15 23:57:38 -0300

    Replace dist with docs directory

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ export default {
   // Path and filename of your result bundle.
   // Webpack will bundle all JavaScript into this file
   output: {
-    path: path.resolve('', 'dist'),
+    path: path.resolve('', 'docs'),
     publicPath: '',
     filename: 'assets/web.js'
   },
